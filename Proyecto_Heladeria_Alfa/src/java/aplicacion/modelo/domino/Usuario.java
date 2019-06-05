24fc90eb7e15c791b50253a02ea50d99b58f00f7
condori jose <Leo@LAPTOP-QF4OQSLS>
2019-06-05 19:58:44 -0300

    clase dominio Usuario

--- Proyecto_Heladeria_Alfa/src/java/aplicacion/modelo/domino/Usuario.java
+++ Proyecto_Heladeria_Alfa/src/java/aplicacion/modelo/domino/Usuario.java
@@ -7,8 +7,65 @@ package aplicacion.modelo.domino;
 
 /**
  *
- * @author Leo
+ * @author Grupo Alfa
  */
 public class Usuario {
+    private Integer codigoUsuario;
+    private String nombreUsuario;
+    private String passwordUsuario;
+    private String tipoUsuario;
+    private boolean estadoUsuario;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer codigoUsuario, String nombreUsuario, String passwordUsuario, String tipoUsuario, boolean estadoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.passwordUsuario = passwordUsuario;
+        this.tipoUsuario = tipoUsuario;
+        this.estadoUsuario = estadoUsuario;
+    }
+
+    public Integer getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    public void setCodigoUsuario(Integer codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPasswordUsuario() {
+        return passwordUsuario;
+    }
+
+    public void setPasswordUsuario(String passwordUsuario) {
+        this.passwordUsuario = passwordUsuario;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public boolean isEstadoUsuario() {
+        return estadoUsuario;
+    }
+
+    public void setEstadoUsuario(boolean estadoUsuario) {
+        this.estadoUsuario = estadoUsuario;
+    }
+    
     
 }
---
+++ Proyecto_Heladeria_Alfa/src/java/aplicacion/modelo/domino/constante/EstadoUsuario.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.modelo.domino.constante;
+
+/**
+ *
+ * @author Grupo Alfa
+ */
+public class EstadoUsuario {
+   public static final boolean FALSE=false;    
+   public static final boolean TRUE=true;    
+}
